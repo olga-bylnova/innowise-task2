84fe23f8d26ff9f7a28ee212140dcd1cc84ae437
olga-bylnova <olga.bylnova@yandex.by>
2023-03-11 14:52:15 +0300

    check note and interval existing

--- src/main/java/com/innowise/Intervals.java
+++ src/main/java/com/innowise/Intervals.java
@@ -11,7 +11,9 @@ public class Intervals {
     private static final String F = "F";
     private static final String G = "G";
     private static final String SHARP = "#";
+    private static final String DOUBLE_SHARP = "##";
     private static final String FLAT = "b";
+    private static final String DOUBLE_FLAT = "bb";
 
     private static final String m2 = "m2";
     private static final String M2 = "M2";
@@ -37,19 +39,46 @@ public class Intervals {
     private static final Integer[] M7_SEMITONE_DEGREE = {11, 7};
     private static final Integer[] P8_SEMITONE_DEGREE = {12, 8};
 
-    private static final List<String> notes = List.of(A, B, C, D, E, F, G);
-    private static final List<String> accidentals = List.of(SHARP, FLAT);
+    private static final String ASCENDING_ORDER = "ASC";
+    private static final String DESCENDING_ORDER = "DSC";
+
+    private static final List<String> notes = List.of(C, D, E, F, G, A, B);
+    private static final List<Integer> semitones = List.of(2, 2, 1, 2, 2, 2, 1);
+    private static final List<String> accidentals = List.of(SHARP, FLAT, "",
+            DOUBLE_SHARP, DOUBLE_FLAT);
+    private static final List<String> building_orders = List.of(ASCENDING_ORDER,
+            DESCENDING_ORDER);
     private static final List<String> intervals = List.of(m2, M2, m3, M3, P4, P5,
             m6, M6, m7, M7, P8);
 
     public static String intervalConstruction(String[] args) {
-        String intervalName = args[0];
-        String firstNote = args[1];
+        int degreeCount = 0, semitoneCount = 0;
+        int index = 0;
+        String intervalName = args[index++];
+        String firstNote = args[index++];
+        String firstNoteWithoutAccidentals = firstNote.substring(0, 1);
+        String firstNoteAccidentals = firstNote.substring(1);
+        String intervalOrder = ASCENDING_ORDER;
 
-        if (!notes.contains(firstNote.substring(0, 1))
-        || !accidentals.contains(firstNote.substring(1, 1))) {
+        if (args.length > index) {
+            intervalOrder = args[index];
+
+            if (!building_orders.contains(intervalOrder.toUpperCase())) {
+                intervalOrder = ASCENDING_ORDER;
+            }
+        }
+
+        if (!notes.contains(firstNoteWithoutAccidentals)
+                || !accidentals.contains(firstNoteAccidentals)) {
             throw new RuntimeException("no such note");
         }
+
+        if (!intervals.contains(intervalName)) {
+            throw new RuntimeException("no such interval");
+        }
+
+
+
 
         return null;
     }
